bc5344ccf6b6de486b7a8870fc72daa150ee138e
Arogya Saakha <aros22@hampshire.edu>
2023-01-30 10:34:15 -0500

    Fixed the problem

--- octacat_modify.R
+++ octacat_modify.R
@@ -4,6 +4,6 @@ octocat = readLines("./octocat.txt")
 
 octocat = c(octocat, "ASCII Art of the Octocat Mascot for Github")
 
-writeChar(octocat, "./octocat.txt")
+writeLines(octocat, "./octocat.txt")
 
 #We use c() here to combine octocat with our caption, and then assign it back to our octocat object.
